a28734b43b84c2b8b41e957dfb8967f62a7f141c
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-28 10:38:17 +0100

    feat: complete form

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,25 +3,67 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import yupSchema from "../validation";
 
 const Form = () => {
-  const { register, handleSubmit, errors } = useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     resolver: yupResolver(yupSchema),
   });
+
+  const submitForm = (data) => {
+    console.log(data);
+  };
 
   return (
     <div className="Form">
       <div className="title">Sign Up</div>
       <div className="inputs">
-        <form>
-          <input type="text" name="firstName" placeholder="First name..." />
-          <input type="text" name="lastName" placeholder="Last name..." />
-          <input type="text" name="email" placeholder="Email..." />
-          <input type="text" name="age" placeholder="Age..." />
-          <input type="text" name="password" placeholder="Password..." />
+        <form onSubmit={handleSubmit(submitForm)}>
+          <input
+            type="text"
+            name="firstName"
+            placeholder="First name..."
+            {...register("firstName")}
+          />
+          <p>{errors.firstName?.message}</p>
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Last name..."
+            {...register("lastName")}
+          />
+          <p>{errors.lastName?.message}</p>
+          <input
+            type="text"
+            name="email"
+            placeholder="Email..."
+            {...register("email")}
+          />
+          <p>{errors.email?.message}</p>
+          <input
+            type="number"
+            min={0}
+            name="age"
+            placeholder="Age..."
+            {...register("age")}
+          />
+          <p>{errors.age?.message}</p>
+          <input
+            type="text"
+            name="password"
+            placeholder="Password..."
+            {...register("password")}
+          />
+          <p>{errors.password?.message}</p>
           <input
             type="text"
             name="confirmPassword"
             placeholder="Confirm password..."
+            {...register("confirmPassword")}
           />
+          <p>{errors.confirmPassword && "Passwords do not match"}</p>
+          <input type="submit" id="submit" />
         </form>
       </div>
     </div>
