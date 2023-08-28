6c8c7ff6f55dcdc98dde6025807ff018b9db5544
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-28 10:41:54 +0100

    feat: customise another error message

--- src/validation.js
+++ src/validation.js
@@ -4,7 +4,7 @@ const yupSchema = yup.object().shape({
   firstName: yup.string().required("First name is required, fool"),
   lastName: yup.string().required(),
   email: yup.string().email().required(),
-  age: yup.number().positive().integer(),
+  age: yup.number().positive("Make it positive, fool").integer(),
   password: yup.string().min(4).max(15).required(),
   confirmPassword: yup
     .string()
